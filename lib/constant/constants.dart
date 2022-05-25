e6ff513f0afe3296cf3be60dcc3682fbf1d8e267
Abood-hass <60661627+Abood-hass@users.noreply.github.com>
2022-05-25 17:41:51 +0300

    finally something work ..almost

---
+++ lib/constant/constants.dart
@@ -0,0 +1,13 @@
+String mapBoxApiKey =
+    'pk.eyJ1IjoiYWJvb2QyMTE5IiwiYSI6ImNsM2xkenNlazAwa2EzZW83ZmZyb25sNGMifQ.N3Cv1emyx9T0inGcbQETtw';
+
+String nftStorageApiKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDY3YjE1NjA1MjJkNGMyNERlMGZkOWU3MTliOWE4MjZBNkI4OTNGYmYiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY1MzQ2NjAyODQ2MSwibmFtZSI6IkFib29kaGFzcyJ9.IGr1cBK8CGbxGrkkxNKPWP5Aie50VE7YB83uPHbrmB8';
+
+const String rpcUrl =
+    "https://ropsten.infura.io/v3/632b7de8dbf84fa2ae7726b15fe0d859";
+//"http://127.0.0.1:7545";
+
+const String contractAddress = "0x702058Ba021Cd4e4F847B40F32b58aA5be3a4661";
+
+const int chainId = 3; //Ropsten-3, Polygon testnet-80001, local ganache-1337
